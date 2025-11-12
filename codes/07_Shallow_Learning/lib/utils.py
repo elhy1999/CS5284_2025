f39676a2701e57985d95f7f587caf3016638b826
E. Liu <ernestliu64@gmail.com>
2025-11-12 12:50:23 +0800

    update

--- codes/07_Shallow_Learning/lib/utils.py
+++ codes/07_Shallow_Learning/lib/utils.py
@@ -60,12 +60,12 @@ def compute_ncut(Adj, R):
 # Laplacian eigenvectors
 def compute_LapEig(g, pos_enc_dim): # input g is a DGL graph
     Adj = g.adj().to_dense() # Adjacency matrix
-    Dn = ( g.in_degrees()** -0.5 ).diag() # Inverse and sqrt of degree matrix
+    Dn = (g.in_degrees()** -0.5).diag() # Inverse and sqrt of degree matrix
     Lap = torch.eye(g.number_of_nodes()) - Dn.matmul(Adj).matmul(Dn) # Laplacian operator
     EigVal, EigVec = torch.linalg.eig(Lap) # Compute full EVD
     EigVal, EigVec = EigVal.real, EigVec.real # make eig real
     EigVec = EigVec[:, EigVal.argsort()] # sort in increasing order of eigenvalues
-    EigVec = EigVec[:,1:pos_enc_dim+1] # select the first non-trivial "pos_enc_dim" eigenvector
+    EigVec = EigVec[:, 1:pos_enc_dim+1] # select the first non-trivial "pos_enc_dim" eigenvector
     return EigVec
 
 
